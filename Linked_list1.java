63209e699261c647196f145138786124d8bec893
Shalin-string <Shalin101918s@gmail.com>
2026-07-03 12:23:59 +0530

    Linkedlist

---
+++ Linked_list1.java
@@ -0,0 +1,46 @@
+public class Linked_list1 {
+    public static void main(String[] args) {
+        sl s = new sl();
+        s.addNode( 10);
+        s.addNode( 20);
+        s.addNode( 30);
+        s.addNode( 30);
+        s.addNode( 30);
+
+        s.display();
+        
+    }
+}
+
+class sl{
+
+    Node head;
+    Node last;
+    void addNode(int data){
+        Node tmp = new Node();
+        tmp.data = data;
+        tmp.next = null;
+        if(head == null){
+            head = tmp;
+            head.data = data;
+            last = tmp;
+        } else {
+            last.next = tmp;
+            last = tmp;
+        }
+    }
+    void display(){
+        Node tmp = head;
+        while(tmp != null){
+            System.out.println(tmp.data);
+            tmp = tmp.next;
+        }
+    }
+}
+
+class Node{
+    int data;
+    Node next;
+    
+
+}
