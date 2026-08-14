f1f0201a2275530e4011c50530d66381b248ad2b
Shalin-string <Shalin101918s@gmail.com>
2026-08-14 12:27:47 +0530

    DFS

--- Graph/GraphDyn.java
+++ Graph/GraphDyn.java
@@ -1,13 +1,14 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-
 public class GraphDyn {
     public static void main(String[] args) {
-        
+
         ArrayList<Edge> edge = new ArrayList<Edge>();
         Scanner src = new Scanner(System.in);
-        for(int i =1; i<=7; i++){
+        System.out.println("Enter no of edges :");
+        int Av_edges = src.nextInt();
+        for (int i = 1; i <= Av_edges; i++) {
             Edge e = new Edge();
             System.out.println("Enter source and destination : ");
             e.src = src.nextInt();
@@ -19,24 +20,41 @@ public class GraphDyn {
             rev.dest = e.src;
             edge.add(rev);
         }
-        System.out.println("Source "+" : "+"Destination");
-        for(int i=0;i<14;i++){
+        System.out.println("Source " + " : " + "Destination");
+        for (int i = 0; i < 14; i++) {
             // System.out.println(edge.get(i).src + " : "+edge.get(i).dest);
 
         }
 
         System.out.println("Enter source :");
         int data = src.nextInt();
-        for(int i=0;i<14;i++){
+        for (int i = 0; i < edge.size(); i++) {
             if (data == edge.get(i).src) {
-                System.out.println(edge.get(i).src + " : "+edge.get(i).dest);
+                System.out.println(edge.get(i).src + " : " + edge.get(i).dest);
+            }
+        }
+        GraphDyn gdma = new GraphDyn();
+        System.out.println("Enter current to start : ");
+        int curr = src.nextInt();
+        ArrayList<Integer> vis = new ArrayList<>();
+        gdma.dfs(curr, vis, edge);
+    }
+
+    void dfs(int current, ArrayList<Integer> visited, ArrayList<Edge> edges) {
+        System.out.println("current visiting vertext : "+current);
+        visited.add(current);
+        for(int i=0; i<edges.size();i++){
+            if (edges.get(i).src == current && !(visited.contains(edges.get(i).dest))) {
+                current = edges.get(i).dest;
+                dfs(current, visited, edges);
+                System.out.println("Current visiting vertext -> rev : "+current);
             }
         }
     }
 
 }
 
-class Edge{
+class Edge {
     int src;
     int dest;
 }
